88469cd81da643b896fa049b761e020505f59ecf
alwil17 <willialfred24@gmail.com>
2025-08-05 01:22:31 +0000

    Add Wallet model to the SQLAlchemy registration in the models module

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,5 +1,6 @@
 # Import all models here to ensure they are registered with SQLAlchemy
 from app.db.models.user import User
 from app.db.models.refresh_token import RefreshToken
+from app.db.models.wallet import Wallet
 
-__all__ = ["User", "RefreshToken"]
+__all__ = ["User", "RefreshToken", "Wallet"]
