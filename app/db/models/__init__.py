e8df33f6a4da6501f34bc4a8d051c5822d5857eb
alwil17 <willialfred24@gmail.com>
2025-08-05 12:25:09 +0000

    Add Transfer model to __all__ in db models initialization

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -5,5 +5,6 @@ from app.db.models.wallet import Wallet
 from app.db.models.transaction import Transaction, TransactionType, TransactionCategory
 from app.db.models.file import File, FileType
 from app.db.models.debt import Debt
+from app.db.models.transfer import Transfer
 
-__all__ = ["User", "RefreshToken", "Wallet", "Transaction", "TransactionType", "TransactionCategory", "File", "FileType", "Debt"]
+__all__ = ["User", "RefreshToken", "Wallet", "Transaction", "TransactionType", "TransactionCategory", "File", "FileType", "Debt", "Transfer"]
