deaf927ec67ff83daac5e15309b08ec06033434b
alwil17 <willialfred24@gmail.com>
2025-08-06 07:53:02 +0000

    Fix import path for Base model in Transfer model definition

--- app/db/models/transfer.py
+++ app/db/models/transfer.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, DateTime, Numeric, ForeignKey, Text
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from app.db.base import Base
+from app.db.models.base import Base
 
 
 class Transfer(Base):
