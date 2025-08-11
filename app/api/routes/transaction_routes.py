4f777c177ca63b5a670edefcd0e335420efac542
alwil17 <willialfred24@gmail.com>
2025-08-11 15:55:11 +0000

    Add CategoryResponse import to transaction_routes.py

--- app/api/routes/transaction_routes.py
+++ app/api/routes/transaction_routes.py
@@ -16,6 +16,7 @@ from app.core.security import get_current_user
 from app.db.base import get_db
 from app.db.models.user import User
 from app.db.models.file import FileType
+from app.schemas.category_dto import CategoryResponse
 from app.services.transaction_service import TransactionService
 from app.schemas.transaction_dto import (
     TransactionCreateDTO,
